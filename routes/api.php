cd05db45bce5e72e0fb64a137f526ae31a84d42d
Don Cadavona <dcadavona@gmail.com>
2018-03-13 15:21:10 +0800

    Fixed orders api route name.

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['namespace' => 'Api', 'middleware' => ['client']], function() {
+Route::group(['namespace' => 'Api', 'middleware' => ['client'], 'as' => 'api.'], function() {
 
 	// test route
 	Route::get('test', function (Request $request) {
