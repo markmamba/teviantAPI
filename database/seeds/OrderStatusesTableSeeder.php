d08876f64ee9f37d73d78802c8814de4cac8b5a3
Don Cadavona <dcadavona@gmail.com>
2018-03-19 23:59:07 +0800

    Added orders truncator for local env and seeder.

--- database/seeds/OrderStatusesTableSeeder.php
+++ database/seeds/OrderStatusesTableSeeder.php
@@ -12,14 +12,29 @@ class OrderStatusesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('order_statuses')->delete();
-
+        // On local environment only
+        if (App::isLocal()) {
+            // Truncate orders and its associated tables
+            Schema::disableForeignKeyConstraints();
+            DB::table('order_billing_addresses')->truncate();
+            DB::table('order_shipping_addresses')->truncate();
+            DB::table('order_carriers')->truncate();
+            DB::table('order_shipments')->truncate();
+            DB::table('order_products')->truncate();
+            DB::table('order_product_reservations')->truncate();
+            DB::table('order_users')->truncate();
+            DB::table('orders')->truncate();
+            DB::table('order_statuses')->truncate();
+            Schema::enableForeignKeyConstraints();
+        }
+        
         $order_statuses = [
-            ['id' => 1, 'name' => 'Pending'],
-            ['id' => 2, 'name' => 'Processed'],
-            ['id' => 3, 'name' => 'Delivered'],
-            ['id' => 4, 'name' => 'Done'],
-            ['id' => 5, 'name' => 'Cancelled'],
+            ['name' => 'Pending'],
+            ['name' => 'Pick Listed'],
+            ['name' => 'Packed'],
+            ['name' => 'Shipped'],
+            ['name' => 'Delivered'],
+            ['name' => 'Cancelled'],
         ];
 
         DB::table('order_statuses')->insert($order_statuses);
