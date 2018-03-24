3d44ab8d286ebb9b1cb6b896416760028bddd56e
Don Cadavona <dcadavona@gmail.com>
2018-03-25 00:15:53 +0800

    Remove debugging codes.

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -65,35 +65,21 @@ class OrderObserver
             'timeout'  => 2.0,
         ]);
     }
-
-    // WIP
+    
     public function updated(Order $order)
     {
         $order_statuses = collect(OrderStatus::orderBy('id', 'asc')->pluck('name', 'id'))->toArray();
+        
         // Get the status Id from request() if its updated via form submit or via programatically.
         $status_name = $order_statuses[isset(request()->status_id) ? request()->status_id : $order->status_id];
-
-        // Debug
-    	// echo '<br>';
-    	// echo 'status_id = ' . request()->status_id . ' | ' . $status_name;
         
         foreach ($this->status_associations as $key => $value) {
-            // Debug
-            // $matched = array_search($status_name, $value);
-            // echo '<br> ';
-            // echo $key . ' => ' . implode($value, ',') . ' | Searched: ' . $matched;
-            
             // Search
             if (in_array($status_name, $value)) {
             	request()->status_id = collect($this->ecommerce_order_statuses)->search($key);
-            	// Debug
-				// echo '<br>';
-				// echo 'Matched = ' . request()->status_id;
                 break;
             }
         }
-        // Debug
-        // die();
 
         // Update the ecommerce through its API.
         try {
