cd107b854ba584e88f2d644f33255c2bfd786e0b
Don Cadavona <dcadavona@gmail.com>
2018-03-26 21:11:51 +0800

    Metrics form validation.

--- app/Http/Requests/MetricRequest.php
+++ app/Http/Requests/MetricRequest.php
@@ -25,7 +25,8 @@ class MetricRequest extends \Backpack\CRUD\app\Http\Requests\CrudRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+            'name'   => 'required|min:2|max:255|unique:metrics,name,' . $this->route('metric'),
+            'symbol' => 'required|min:1|max:3|unique:metrics,name,' . $this->route('metric'),
         ];
     }
 
