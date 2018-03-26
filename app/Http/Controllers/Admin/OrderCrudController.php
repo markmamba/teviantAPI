92cfe3db699099ea9136bbdd46076fead64378c3
Don Cadavona <dcadavona@gmail.com>
2018-03-27 01:27:41 +0800

    Minor cleanup.

--- app/Http/Controllers/Admin/OrderCrudController.php
+++ app/Http/Controllers/Admin/OrderCrudController.php
@@ -407,8 +407,6 @@ class OrderCrudController extends CrudController
     {
         $order = Order::findOrFail($id);
 
-        // return view('pdf.pick_list', compact('order'));
-
         $pdf = \PDF::loadView('pdf.all', compact('order'));
         return $pdf->stream();
     }
