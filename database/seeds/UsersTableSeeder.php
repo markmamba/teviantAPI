a078762e5a4e4c1f37a28987c494a1adc189af9e
Don Cadavona <dcadavona@gmail.com>
2018-03-31 15:20:12 +0800

    Added initial users table seeder.

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	if (App::isLocal()) {
+            // Truncate orders and its associated tables
+            Schema::disableForeignKeyConstraints();
+            DB::table('order_billing_addresses')->truncate();
+            DB::table('order_shipping_addresses')->truncate();
+            DB::table('order_carriers')->truncate();
+            DB::table('order_shipments')->truncate();
+            DB::table('order_products')->truncate();
+            DB::table('order_product_reservations')->truncate();
+            DB::table('order_users')->truncate();
+            DB::table('orders')->truncate();
+            DB::table('order_statuses')->truncate();
+            DB::table('users')->truncate();
+            Schema::enableForeignKeyConstraints();
+        }
+
+    	$default_admin_password = null !== env('DEFAULT_ADMIN_PASSWORD') ? env('DEFAULT_ADMIN_PASSWORD') : 'password';
+
+        $users = [
+            [
+            	'name' => 'Teviant Admin',
+            	'email' => 'admin@teviant.com',
+            	'password' => bcrypt($default_admin_password),
+        	],
+        	[
+            	'name' => 'Outbound Operator',
+            	'email' => 'outbound@teviant.com',
+            	'password' => bcrypt($default_admin_password),
+        	],
+        	[
+            	'name' => 'Inbound Operator',
+            	'email' => 'inbound@teviant.com',
+            	'password' => bcrypt($default_admin_password),
+        	],
+        ];
+
+        DB::table('users')->insert($users);
+    }
+}
