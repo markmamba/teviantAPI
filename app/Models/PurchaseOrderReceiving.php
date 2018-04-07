2871c3397fbc40d2495d6eaf82f57c426aec9816
Don Cadavona <dcadavona@gmail.com>
2018-04-08 03:20:31 +0800

    Fix translationEnabled() issue.

--- app/Models/PurchaseOrderReceiving.php
+++ app/Models/PurchaseOrderReceiving.php
@@ -2,18 +2,20 @@
 
 namespace App\Models;
 
-use App\Models\PurchaseOrder;
+use Backpack\CRUD\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class PurchaseOrderReceiving extends Model
 {
+    use CrudTrait;
+
     public function product()
     {
-    	return $this->belongsTo('App\Models\PurchaseOrderProduct', 'product_id');
+        return $this->belongsTo('App\Models\PurchaseOrderProduct', 'product_id');
     }
 
     public function purchase_order()
     {
-    	return $this->belongsTo('App\Models\PurchaseOrder');
+        return $this->belongsTo('App\Models\PurchaseOrder');
     }
 }
