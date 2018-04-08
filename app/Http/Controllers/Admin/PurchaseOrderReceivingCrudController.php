986284b665e67338d99797e7c9fe88261aa48447
Don Cadavona <dcadavona@gmail.com>
2018-04-08 13:53:25 +0800

    Minor code reformat.

--- app/Http/Controllers/Admin/PurchaseOrderReceivingCrudController.php
+++ app/Http/Controllers/Admin/PurchaseOrderReceivingCrudController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Models\PurchaseOrderProductReceiving;
-use Backpack\CRUD\app\Http\Controllers\CrudController;
-
-// VALIDATION: change the requests to match your own file names if you need form validation
 use App\Http\Requests\ReceivingRequest as StoreRequest;
 use App\Http\Requests\ReceivingRequest as UpdateRequest;
+
+// VALIDATION: change the requests to match your own file names if you need form validation
+use Backpack\CRUD\app\Http\Controllers\CrudController;
 
 class PurchaseOrderReceivingCrudController extends CrudController
 {
@@ -17,7 +16,7 @@ class PurchaseOrderReceivingCrudController extends CrudController
         |--------------------------------------------------------------------------
         | BASIC CRUD INFORMATION
         |--------------------------------------------------------------------------
-        */
+         */
         $this->crud->setModel('App\Models\PurchaseOrderReceiving');
         $this->crud->setRoute(config('backpack.base.route_prefix') . '/receiving');
         $this->crud->setEntityNameStrings('Receiving', 'Receivings');
@@ -26,27 +25,27 @@ class PurchaseOrderReceivingCrudController extends CrudController
         |--------------------------------------------------------------------------
         | BASIC CRUD INFORMATION
         |--------------------------------------------------------------------------
-        */
-       
-       $this->crud->setFromDb();
+         */
+
+        $this->crud->setFromDb();
 
         // ------ CRUD FIELDS
         // $this->crud->child_resource_included = ['select' => false, 'number' => false];
         $this->crud->addFields([
             [
-               'label'     => 'Purchase Order #',
-               'type'      => 'select2',
-               'name'      => 'purchase_order_id',
-               'entity'    => 'purchase_order',
-               'attribute' => 'id',
-               'model'     => 'App\Models\PurchaseOrder',
-            ]
+                'label'     => 'Purchase Order #',
+                'type'      => 'select2',
+                'name'      => 'purchase_order_id',
+                'entity'    => 'purchase_order',
+                'attribute' => 'id',
+                'model'     => 'App\Models\PurchaseOrder',
+            ],
         ]);
 
         // ------ CRUD COLUMNS
 
         // ------ CRUD BUTTONS
-        
+
         // ------ CRUD ACCESS
 
         // ------ CRUD REORDER
