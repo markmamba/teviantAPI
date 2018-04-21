f59c2b2313be0dd80844ae6f12c641d329426a92
Don Cadavona <dcadavona@gmail.com>
2018-04-21 12:47:04 +0800

    fix previous routing setting.

--- resources/views/admin/purchase_orders/receivings/create.blade.php
+++ resources/views/admin/purchase_orders/receivings/create.blade.php
@@ -8,7 +8,7 @@
 	  </h1>
 	  <ol class="breadcrumb">
 	    <li><a href="{{ url(config('backpack.base.route_prefix'), 'dashboard') }}">{{ trans('backpack::crud.admin') }}</a></li>
-	    <li><a href="{{ url($crud->route) }}" class="text-capitalize">{{ $crud->entity_name_plural }}</a></li>
+	    <li><a href="{{ route('crud.purchase-order.index') }}" class="text-capitalize">{{ $crud->entity_name_plural }}</a></li>
 	    <li class="active">{{ trans('backpack::crud.add') }}</li>
 	  </ol>
 	</section>
@@ -19,7 +19,7 @@
 	<div class="col-md-8 col-md-offset-2">
 		<!-- Default box -->
 		@if ($crud->hasAccess('list'))
-			<a href="{{ url($crud->route) }}"><i class="fa fa-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a><br><br>
+			<a href="{{ route('crud.purchase-order.index') }}"><i class="fa fa-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a><br><br>
 		@endif
 
 		@include('crud::inc.grouped_errors')
