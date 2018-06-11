c8c70d5a33a71c92068bc9e2add2b4f2d5c16a48
Don Cadavona <dcadavona@gmail.com>
2018-06-11 23:07:29 +0800

    Just use 10 stocks in seed.

--- database/seeds/InventoryStocksTableSeeder.php
+++ database/seeds/InventoryStocksTableSeeder.php
@@ -21,7 +21,7 @@ class InventoryStocksTableSeeder extends Seeder
                 collect([
                     'inventory_id' => $inventory->id,
                     'location_id'  => Location::inRandomOrder()->first()->id,
-                    'quantity'     => collect([5, 10, 50, 100, 200])->random(),
+                    'quantity'     => 10,
                     'aisle'        => '1',
                     'row'          => '2',
                     'bin'          => '3',
