f949ce697103daf7573ac1c12e1527a8621939e4
Don Cadavona <dcadavona@gmail.com>
2018-12-10 14:48:13 +0800

    Rename command.

--- app/Console/Commands/SyncCategories.php
+++ app/Console/Commands/SyncCategories.php
@@ -14,7 +14,7 @@ class SyncCategories extends Command
      *
      * @var string
      */
-    protected $signature = 'categories:sync';
+    protected $signature = 'sync:categories';
 
     /**
      * The console command description.
